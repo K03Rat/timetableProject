1f255a770049ec51bc10a5db28a08d23c3c885ae
Kristian <kristianratnikov28@gmail.com>
2024-10-06 22:30:32 +0100

    Refactor Lecturer  model: Update field types and simplify constructors

--- src/main/java/com/group16/timetable/model/Lecturer.java
+++ src/main/java/com/group16/timetable/model/Lecturer.java
@@ -1,110 +1,91 @@
 package com.group16.timetable.model;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
+import java.util.List;
+
 
 @Entity
 @Table(name = "lecturers")
 public class Lecturer {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-	private Long id;
-	
-	@Column(name = "first_name", nullable = false)
+    private Long id;
+
+    @JsonProperty("firstName") // Optional: helps with JSON mapping
     private String firstName;
 
-    @Column(name = "last_name", nullable = false)
+    @JsonProperty("lastName") // Optional: helps with JSON mapping
     private String lastName;
 
     @Column(name = "email")
     private String email;
-	
-	@ManyToMany(mappedBy = "lecturers")
-	@JsonIgnore
-	private List<Modules> modules;
-	
-	// Default Constructor
-	public Lecturer() {
-		super();
-	}
-	
-	// Constructor with fields
-	public Lecturer(String firstName, String lastName, String email) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
-	
-	// Constructor with timetables
-	public Lecturer(String firstName, String lastName, String email, List<Modules> modules) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.modules = modules;
-	}
 
-	// Getters and Setters
-	public String getEmail() {
-		return email;
-	}
-	
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	
-	public String getLastName() {
-		return lastName;
-	}
-	
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	public Long getId() {
-		return id;
-	}
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	public List<Modules> getTimetables() {
-		return modules;
-	}
+    @ManyToMany
+    @JoinTable(name = "lecturer_module", joinColumns = @JoinColumn(name = "lecturer_id"), inverseJoinColumns = @JoinColumn(name = "module_id"))
+    @JsonIgnore
+    private List<Modules> modules;
 
-	public void setTimetables(List<Modules> timetables) {
-		this.modules = timetables;
-	}
+    // Default Constructor
+    public Lecturer() {
+        super();
+    }
+
+    // Constructor with fields
+    public Lecturer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = generateEmail(firstName, lastName); // This ensures the email is populated
+    }
+
+    private String generateEmail(String firstName, String lastName) {
+        return (firstName.charAt(0) + "." + lastName + "@universityofgalway.ie").toLowerCase();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
 
-	@Override
-	public String toString() {
-		return "Lecturer{" +
-		       "id=" + id +
-		       ", firstName='" + firstName + '\'' +
-		       ", lastName='" + lastName + '\'' +
-		       ", email='" + email + '\'' +
-		       '}';
-	}
+    public List<Modules> getModules() {
+        return modules;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Lecturer{" + "id=" + id + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", email='" + email + '\'' + '}';
+    }
 }
