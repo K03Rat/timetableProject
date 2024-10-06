b9dce119bd8e33a77591abb7d53fe5cb73af26e2
Kristian <kristianratnikov28@gmail.com>
2024-10-06 22:29:42 +0100

    Refactor Course model: Update field types and simplify constructors

--- src/main/java/com/group16/timetable/model/Course.java
+++ src/main/java/com/group16/timetable/model/Course.java
@@ -1,63 +1,56 @@
 package com.group16.timetable.model;
 
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
 import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
-import jakarta.persistence.JoinColumn;
 @Entity
 @Table(name = "courses")
 public class Course {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private Long id;
-	
-	@Column(name="course_code")
-	private String code;
-	
-	@ManyToMany
-	@JoinTable(
-	    name = "course_modules",
-	    joinColumns = @JoinColumn(name="course_id"),
-	    inverseJoinColumns = @JoinColumn(name="module_id")
-	)
-	@JsonManagedReference
-	private List<Modules> modules;
-	
-	
-	public Course() {
-		super();
-	}
-	public Course(String code, List<Modules> modules) {
-		this.code = code;
-		this.modules = modules;
-	}
-	
-	public List<Modules> getCourseModules(){
-		return modules;
-	}
-	
-	public String getCourseCode() {
-		return code;
-	}
-	
-	public Long getCourseId() {
-		return id;
-	}
-	
-	
 
-	
-	
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+
+    @Column(name = "course_code")
+    private String code;
+
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "course_modules",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "module_id")
+    )
+    private Set<Modules> modules;
+
+    public Course() {
+        super();
+    }
+
+    public Course(String code) {
+        this.code = code;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Set<Modules> getCourseModules() {
+        return modules;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
