50835c056d5bc4cdfe10be685e93acadfc7cbb7d
Kristian <kristianratnikov28@gmail.com>
2024-10-06 22:35:01 +0100

    Refactor:Add methods in LecturerRepository

--- src/main/java/com/group16/timetable/repositories/LecturerRepository.java
+++ src/main/java/com/group16/timetable/repositories/LecturerRepository.java
@@ -8,9 +8,19 @@ import org.springframework.stereotype.Repository;
 import com.group16.timetable.model.Lecturer;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LecturerRepository extends JpaRepository<Lecturer, Long> {
 
-    
+    // Optional method to find lecturer by email
+    Optional<Lecturer> findByEmail(String email);
+
+    // SQL query to get timetable details for a lecturer based on the modules they teach
+    @Query(value = "SELECT t.semester, t.day AS day, t.start_time AS time, t.venue AS venue, m.module_code AS module_code, m.module_name AS module_name " +
+            "FROM timetables t " +
+            "JOIN lecturer_module lm ON t.module_id = lm.module_id " +
+            "JOIN modules m ON lm.module_id = m.id " +
+            "WHERE lm.lecturer_id = :lecturerId", nativeQuery = true)
+    List<Object[]> findTimetableDetailsByLecturerId(@Param("lecturerId") Long lecturerId);
 }
